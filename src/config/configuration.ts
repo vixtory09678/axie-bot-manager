2f9221a01ad3a74d7de9ba988a2bd305d15b8882
vixotory09678 <robotics.za@gmail.com>
2022-05-01 02:16:05 +0700

    used secret from secret manager

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,1 +1,24 @@
-import secrets from '../../.key.json';export default () => ({  secrets,});
+import { SecretsManager, Credentials } from 'aws-sdk';
+const REGION = 'ap-southeast-1';
+const SECRET_NAME = 'axie/secret';
+
+async function getSecretManager(secretName: string) {
+  const client = new SecretsManager({
+    region: REGION,
+    credentials: new Credentials(
+      process.env.ACCESS_KEY_ID,
+      process.env.SECRET_ACCESS_KEY,
+    ),
+  });
+
+  const secret = await client
+    .getSecretValue({ SecretId: secretName })
+    .promise();
+
+  const arraySecret = Object.values(JSON.parse(secret.SecretString));
+  return arraySecret;
+}
+
+export default async () => ({
+  secret: await getSecretManager(SECRET_NAME),
+});
